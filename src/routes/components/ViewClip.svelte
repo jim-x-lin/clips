5a0ff875e872007f96ec8a1307f144c0b15a549c
Jim X. Lin <jim@jimxlin.com>
2023-01-10 16:15:11 -0800

    Create dedicated edit button

--- src/routes/components/ViewClip.svelte
+++ src/routes/components/ViewClip.svelte
@@ -5,12 +5,16 @@
 	export let clip: ClipType;
 	export let editClipId: string | undefined;
 
-	function handleClick() {
+	function handleClickEdit() {
 		editClipId = clip.id;
 	}
 </script>
 
 <div class="my-4 flex divide-x divide-solid rounded-md border border-gray-400 hover:outline">
-	<button class="flex-1 truncate px-2 py-1 text-left" on:click={handleClick}>{clip.content}</button>
+	<div class="flex-1 truncate px-2 py-1">{clip.content}</div>
+	<button
+		class="w-16 px-2 py-1 text-sm font-semibold text-black hover:bg-gray-400 hover:text-white"
+		on:click={handleClickEdit}>edit</button
+	>
 	<CopyToClipboard content={clip.content} />
 </div>
